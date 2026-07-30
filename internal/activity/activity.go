d6d5fefea5e33dc97f52212506be25990debace0
ctrl-shift-markus <182812049+ctrl-shift-markus@users.noreply.github.com>
2026-07-30 20:36:16 +0100

    chore: rename FormatDuration parameter

--- internal/activity/activity.go
+++ internal/activity/activity.go
@@ -10,17 +10,17 @@ type Item struct {
 }
 
 // FormatDuration formats a duration in minutes into a formatted, human-readable string
-func FormatDuration(durationInMin int) string {
+func FormatDuration(duration int) string {
 	switch {
-	case durationInMin <= 0:
+	case duration <= 0:
 		return "0m"
-	case durationInMin < 60:
-		return fmt.Sprintf("%dm", durationInMin)
-	case durationInMin%60 == 0:
-		return fmt.Sprintf("%dh", durationInMin/60)
+	case duration < 60:
+		return fmt.Sprintf("%dm", duration)
+	case duration%60 == 0:
+		return fmt.Sprintf("%dh", duration/60)
 	default:
-		hours := durationInMin / 60
-		minutes := durationInMin % 60
+		hours := duration / 60
+		minutes := duration % 60
 		return fmt.Sprintf("%dh %dm", hours, minutes)
 
 	}
